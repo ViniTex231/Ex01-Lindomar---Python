ed0eb9eac8dcdbf52969903dd5e449fcd5798317
ViniTex231 <126170717+ViniTex231@users.noreply.github.com>
2023-03-02 08:09:53 -0300

    Update Ex01 - Lindomar.py

--- Ex01 - Lindomar.py
+++ Ex01 - Lindomar.py
@@ -1,16 +1,21 @@
 num_list = []
 
 i = 0
-maiornum = 0
-menornum = 100000000
+
 while i < 5:
     num = int(input("Digite um número: "))
     num_list.append(num)
     i += 1
-    if num > maiornum:
-        maiornum = num
-    if num < menornum:
-        menornum = num
+
+listord = num_list[:]
+listord.sort()
+menornum = listord[0]
+maiornum = listord[-1]
+
+posmaior = num_list.index(maiornum) + 1
+posmenor = num_list.index(menornum) + 1
+
 print(f"Os números digitados são: {num_list}")
-print(f"O maior número é: {maiornum}")
-print(f"O menor número é: {menornum}")
+print(f"O maior número é: {maiornum} na posição: {posmaior}")
+print(f"O menor número é: {menornum} na posição: {posmenor}")
+
